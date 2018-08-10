9901bfa2addb634139206366c14b283dfec29112
vin <sokthavykoun@gmail.com>
2018-08-10 10:00:41 +0700

    day5 finish log cat check user3

--- app/src/main/java/masterung/androidthai/in/th/laosunseen/fragment/ServiceFragment.java
+++ app/src/main/java/masterung/androidthai/in/th/laosunseen/fragment/ServiceFragment.java
@@ -5,16 +5,25 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.security.cert.Extension;
+import java.util.Map;
 
 import masterung.androidthai.in.th.laosunseen.MainActivity;
 import masterung.androidthai.in.th.laosunseen.R;
@@ -23,6 +32,13 @@ import masterung.androidthai.in.th.laosunseen.R;
 
 
 public class ServiceFragment extends Fragment  {
+    //code ny man code pa kar to pea
+    private String nameString, currentPostString, uidString;
+
+    //code ny man karn sang to pea jai jub Log cat puea check erro
+    private String tag = "10AUGV1";
+
+
 
 
     @Override
@@ -31,7 +47,71 @@ public class ServiceFragment extends Fragment  {
 
 
 
+        FindMyMe();
+
+//        Post Controler
+
+
+        postControler();
+
+
     }     //Main Method
+
+    private void FindMyMe() {
+
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        uidString = firebaseAuth.getCurrentUser().getUid();
+        Log.d(tag, "uid ==>" + uidString);
+
+
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+        DatabaseReference databaseReference = firebaseDatabase.getReference().child("User").child(uidString);
+
+
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                Map map = (Map) dataSnapshot.getValue();
+                nameString = String.valueOf(map.get("nameString"));
+                currentPostString = String.valueOf(map.get("myPosting"));
+                Log.d(tag, "Name ==> " + nameString);
+                Log.d(tag, "currentPost ==>" + currentPostString);
+
+               // Toast.makeText(getContext(), "Name ==> " + nameString, Toast.LENGTH_SHORT).show();
+
+
+
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+
+
+    }
+
+    // Ta sy aw button teow t 1 leo click Control+Space = karn sang Overide
+    private void postControler() {
+        Button button = getView().findViewById(R.id.btnPost);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+
+
+
+            }
+        });
+    }
 
 
     @Nullable
